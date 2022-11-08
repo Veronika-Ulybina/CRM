69db5db9d50e9e8778100ce50e0383a9a44b1c1a
Veronika Ulybina <veronica.ulybina@yandex.ru>
2022-11-09 04:35:52 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const navSystemName = 'Навигационная система Soundmax';
+
+console.log(navSystemName);
+
+const desktopGameName = 'Настольная игра “На 4-х ногах”';
+
+console.log(desktopGameName);
+
+const amountNavSystem = 5;
+
+const amountDesktopGame = 12;
+
+const categoryTechnic = 'Техника для дома';
+
+const categoryGames = 'Настольные игры';
+
+const priceNavSystem = 100;
+
+const priceDesktopGame = 14;
